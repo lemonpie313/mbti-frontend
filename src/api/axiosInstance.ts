80db97bcb0fcef37037f3858269d08da60a815ab
SuhyeonJin <lofi0613@gmail.com>
2025-08-15 17:56:09 +0900

    fin

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,19 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://13.124.137.5:3000",
-  timeout: 5000,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
 });
 
 export default api;
